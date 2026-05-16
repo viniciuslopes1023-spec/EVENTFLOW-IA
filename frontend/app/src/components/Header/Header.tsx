781b6fbcfb0bbb2ce7fa760f5d0e4c2815f789e6
vinciuslopes1023-spec <vinciuslopes1023@email.com>
2026-05-16 20:16:29 -0300

    feat: create landing page header

---
+++ frontend/app/src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { Button } from '../Button/Button';
+import '.header.css';
+
+export function Header(){
+    return(
+        <header className="header">
+            <strong className="header-logo">EventFlow IA </strong>
+            
+
+            <nav className="header-nav">
+                <a href="#features">Recursos</a>
+                <a href="#finances">Financeiro</a>
+                <a href="#ai">IA</a>
+            </nav>
+
+            <Button text="Entrar" variant='secondary'></Button>
+        </header>
+    )
+}
