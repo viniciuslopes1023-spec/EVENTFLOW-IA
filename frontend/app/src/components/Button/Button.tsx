b4d7960687e31a87f0e278b302b2c8777284ef0a
vinciuslopes1023-spec <vinciuslopes1023@email.com>
2026-05-22 11:15:53 -0300

    feat: create reusable Button component with variants

--- frontend/app/src/components/Button/Button.tsx
+++ frontend/app/src/components/Button/Button.tsx
@@ -2,15 +2,20 @@ import './button.css';
 
 type ButtonProps = {
   text: string;
+  type?: 'button' | 'submit'
   variant?: 'primary' | 'secondary'
   onClick?: () => void;
 };
 
-export function Button({ text, variant = 'primary', onClick }: ButtonProps) {
+export function Button({
+  text,
+  type = 'button',
+  variant = 'primary',
+  onClick,
+}: ButtonProps) {
   return (
-    <button className={`button button-${variant}`} onClick={onClick}>
+    <button className={`button button-${variant}`} type={type} onClick={onClick}>
       {text}
     </button>
-  );
-}
-
+  )
+}  
